a8711404446943a1c38dd05f6d3ee77f04357140
ashalfarhan <ashalfarhan8@gmail.com>
2024-11-12 18:03:30 +0700

    added checker to prevent error

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+// @ts-check
 chrome.runtime.onInstalled.addListener(() => {
   chrome.action.setBadgeText({
     text: 'OFF',
@@ -5,29 +6,31 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.action.onClicked.addListener(async tab => {
+  if (!tab.id) return;
+  if (tab.url && !tab.url.startsWith('http')) return;
   const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-  const { blurRadius } = await chrome.storage.local.get('blurRadius');
-  const css = `body { filter: blur(${blurRadius}px); }`;
+  const { blurRadius = 10 } = await chrome.storage.local.get('blurRadius');
+  const css = `body { filter: blur(${blurRadius}px) !important; }`;
   const nextState = prevState === 'ON' ? 'OFF' : 'ON';
-  console.log({ prevState, nextState });
+  // console.log({ blurRadius, prevState, nextState });
   await chrome.action.setBadgeText({
     tabId: tab.id,
     text: nextState,
   });
 
   if (nextState === 'ON') {
-    console.log('Start injecting CSS');
+    // console.log('Start injecting CSS');
     await chrome.scripting.insertCSS({
       css,
       target: { tabId: tab.id },
     });
-    console.log('Done injecting CSS');
+    // console.log('Done injecting CSS');
   } else if (nextState === 'OFF') {
-    console.log('Start remove CSS');
+    // console.log('Start remove CSS');
     await chrome.scripting.removeCSS({
       css,
       target: { tabId: tab.id },
     });
-    console.log('Done remove CSS');
+    // console.log('Done remove CSS');
   }
 });
